39c429182472439dc615ab98e2a3ad8df6204f97
SamsunNaher-1604048 <samsun.naher.nishi.2017@gmail.com>
2023-04-15 06:25:37 +0600

    setup+folder structure

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import TextField from '@mui/material/TextField';
+const Login = () => {
+  return (
+    <div>
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+        <TextField id="standard-basic" label="Standard" variant="standard" />
+    </div>
+  )
+}
+
+export default Login
